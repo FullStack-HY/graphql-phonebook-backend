9d7b869ab1a04bad6e881a9d8fb1f3d169fc387a
Matti Luukkainen <mluukkai@iki.fi>
2021-02-21 12:42:05 +0200

    part8-3

--- index.js
+++ index.js
@@ -36,11 +36,15 @@ const typeDefs = gql`
     street: String!
     city: String! 
   }
-
+  
+  enum YesNo {
+    YES
+    NO
+  }
 
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 
@@ -51,13 +55,24 @@ const typeDefs = gql`
       street: String!
       city: String!
     ): Person
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
   }  
 `
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons
+      }
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) =>
       persons.find(p => p.name === args.name)
   },
@@ -80,7 +95,17 @@ const resolvers = {
       const person = { ...args, id: uuid() }   
       persons = persons.concat(person)
       return person
-    }
+    },
+    editNumber: (root, args) => {
+      const person = persons.find(p => p.name === args.name)
+      if (!person) {
+        return null
+      }
+  
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+      return updatedPerson
+    }  
   }
 }
 
