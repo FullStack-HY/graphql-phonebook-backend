f15463b57d19b00362e63f1c862880d96c1f19ec
Matti Luukkainen <mluukkai@iki.fi>
2021-02-22 21:49:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { v1: uuid } = require('uuid')
 const mongoose = require('mongoose')
 const Person = require('./models/person')
 
-const MONGODB_URI = 'mongodb+srv://fullstack:fullstack@cluster0.ostce.mongodb.net/fs2021-phonebook-part8?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://fullstack:secred@cluster0.ostce.mongodb.net/fs2021-phonebook-part8?retryWrites=true&w=majority'
 
 console.log('connecting to', MONGODB_URI)
 
@@ -15,29 +15,6 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-  },
-  {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-  },
-  {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-  },
-]
 
 const typeDefs = gql`
   type Person {
@@ -76,55 +53,6 @@ const typeDefs = gql`
     ): Person
   }  
 `
-
-/*
-const resolvers = {
-  Query: {
-    personCount: () => persons.length,
-    allPersons: (root, args) => {
-      if (!args.phone) {
-        return persons
-      }
-      const byPhone = (person) =>
-        args.phone === 'YES' ? person.phone : !person.phone
-      return persons.filter(byPhone)
-    },
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
-  },
-  Person: {
-    address: (root) => {
-      return { 
-        street: root.street,
-        city: root.city
-      }
-    }
-  },
-  Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find(p => p.name === args.name)) {
-        throw new UserInputError('Name must be unique', {
-          invalidArgs: args.name,
-        })
-      }
-
-      const person = { ...args, id: uuid() }   
-      persons = persons.concat(person)
-      return person
-    },
-    editNumber: (root, args) => {
-      const person = persons.find(p => p.name === args.name)
-      if (!person) {
-        return null
-      }
-  
-      const updatedPerson = { ...person, phone: args.phone }
-      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
-      return updatedPerson
-    }  
-  }
-}
-*/
 
 const resolvers = {
   Query: {
